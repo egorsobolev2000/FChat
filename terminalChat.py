de73357f5ad134a086d3a93d383f620f173b2680
root <root@localhost.localdomain>
2019-07-24 01:11:07 +0300

    fist version

--- terminalChat.py
+++ terminalChat.py
@@ -1,27 +1,35 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-clear
 import getpass
+import pysher
+import os
+import json
+from pusher import Pusher
 from termcolor import colored
 from dotenv import load_dotenv
-load_dotenv(dotenv_path = '.evn')
+
+load_dotenv(dotenv_path='.env')
 
 class terminalChat():
-    pusher = None
-    channel = None
-    cahtroom = None
-    clienr_pusher = None
-    user = None
+    pusher = None       #Содержит экземпляр сервера
+    channel = None      #Содержит экземпляр Pusher канала
+    cahtroom = None     #Название канала
+    clientPusher = None #будет содержать экземпляр клиента Pusher
+    user = None         #информацию о текущем вошедшем в систему пользователе
     users = {
     "egor": "egor'spassword"
     "sam": "sam'spassword"
     }
     chatrooms = ['it', 'summer']
 
-    #Точка входа в чат
+    #точка входа в наше приложение
     def main(self):
         self.login()
         self.selectChatroom()
         while True:
             self.getInput()
-    #Валидация даных для входа в систему
+
+    #функция входа в систему
     def login(self):
         username = input("Please enter your name: ")
         password = getpass.getpass("Please enter %s's Password: " % username)
@@ -31,6 +39,7 @@ class terminalChat():
             else:
                  print(colored("Your password is incorrect", "red"))
                 self.login()
+
     #Выбор чата из предоставленного списка
     def slelctChatroom(self):
         print("Available chatrooms are %s" %str(self.chatrooms))
@@ -41,8 +50,36 @@ class terminalChat():
         else:
             print(colored("No such chatroom in our list", "red"))
             self.selectChatroom()
+
+    #Функция инициализации
+    def initPusher(self):
+        self.pusher = Pusher(app_id = os.getenv('PUSHER_APP_ID', None),
+        key = os.getenv('PUSHER_APP_KEY', None),
+        secret = os.getenv('PUSHER_APP_SECRET', None),
+        cluster = os.getenv('PUSHER_APP_CLUSTER', None))
+        # инициализирую новый Pysher клиент передавая APP_KEY
+        self.clientPusher = pysher.Pusher(os.getenv('PUSHER_APP_KEY', None), os.getenv('PUSHER_APP_CLUSTER', None))
+        # Связываюсь с соеденением событием и передаю connectHandler в качестве обратного вызова
+        self.clientPusher.connection.bind('pusher:connection_established', self.connectHandler) #получаю аргумент с именем data
+        self.clientPusher.connect()
+
+    #Её вызывать когда Pusher установил соеденение
+    def connectHandler(self, data):
+        self.channel = self.clientPusher.subscribe(self.chatroom)
+        self.channel.bind('newmessage', self.pusherCallback)
+
+    #когда происходит новое событие
+    def pusherCallback(self, message):
+        message = json.loads(message)
+        if message['user'] != self.user:
+            print(colored("{}: {}".format(message['user']), message['message']), "blue")
+            print(colored("{}: ".format(self.user), "green"))
+
+    # Функция для получения текущего сообщения
     def getInput(self):
         message = input(colored("{}: " .format(self.user), "green")
+        self.pusher.trigger(self.chatroom, u'newmessage', {"user": self.user, "message": message})
+
 
 if __name__ == "__main__":
     terminalChat().main()
