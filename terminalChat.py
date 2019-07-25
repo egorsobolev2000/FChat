abd436e9d528d416bff1ccbc6449bdf7a276eda1
root <root@localhost.localdomain>
2019-07-25 20:04:46 +0300

    Пофиксил бак с выводом сообщения, это был какой то ад

--- terminalChat.py
+++ terminalChat.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-clear
 import getpass
+import pyautogui
 from termcolor import colored
 from pusher import Pusher
 import pysher
 from dotenv import load_dotenv
 import os
 import json
-
-from easterEggs import *
-from size import *
+import sys
+from intro import *
 
 # NOTE: Добавить к сообщения время отправки
 
@@ -19,6 +19,8 @@ def cls():
 cls()
 
 load_dotenv(dotenv_path='.env')
+screen_code = "\033[1A\033[2K"
+
 
 class terminalChat():
     pusher = None       #Содержит экземпляр сервера
@@ -27,10 +29,10 @@ class terminalChat():
     clientPusher = None #будет содержать экземпляр клиента Pusher
     user = None         #информацию о текущем вошедшем в систему пользователе
     users = {
-        "egor": "00000",
-        "user": "user"
+        "tad": "tad",
+        "sam": "sam"
     }
-    chatrooms = ['Somechat', 'Another', 'Some']
+    chatrooms = ['Somechat', 'Another', '1']
 
     #точка входа в приложение
     def main(self):
@@ -47,18 +49,20 @@ class terminalChat():
         username = input("Username: ")
         password = getpass.getpass("Enter %s's Password: " % username)
         if username in self.users:
+            cls()
             if self.users[username] == password:
                 self.user = username
             else:
+                cls()
                 print(colored("Your password is incorrect", "red"))
                 self.login()
         else:
+            cls()
             print(colored("Your username is incorrect", "red"))
             self.login()
 
     #Выбор чата из предоставленного списка
     def selectChatroom(self):
-        cls()
         intro()
         i = 0
         for chat in self.chatrooms:
@@ -66,13 +70,15 @@ class terminalChat():
             print([i], colored(chat, "blue").center(10))
         chatroom = input(colored("\nSelect a chat: ", "green"))
         if chatroom in self.chatrooms:
+            cls()
             self.chatroom = chatroom
             self.initPusher()
         else:
+            cls()
             print(colored("No such chat in our list", "red"))
             self.selectChatroom()
         cls()
-        print("\x1b[6;30;42m", chatroom, "\x1b[0m\n")
+        print("\x1b[6;30;42m","Chat [ {} ]\x1b[0m\n".format(chatroom))
 
     #Функция инициализации
     def initPusher(self):
@@ -88,18 +94,28 @@ class terminalChat():
         self.channel = self.clientPusher.subscribe(self.chatroom)
         self.channel.bind('newmessage', self.pusherCallback)
 
-
-    #когда происходит новое событие
+    #когда происходит новое сообщение
     def pusherCallback(self, message):
         message = json.loads(message)
         if message['user'] != self.user:
-            print(colored("{}: {}".format(message['user'], message['message']), "blue"))
-            print(colored("{}: ".format(self.user), "green"))
+            if message['user'] != self.user:
+                sys.stdout.write('\033[1A')
+                print("\n\x1b[1;34m{}\x1b[0m: {}".format(message['user'], message['message']))
+            print("\x1b[1;31m{}\x1b[0m: ".format(self.user))
+            sys.stdout.write('\n\033[1A')
+            #pyautogui.press('esc')
+            if message['user'] != self.user:
+                sys.stdout.write(screen_code)
 
     # Функция для получения текущего сообщения
     def getInput(self):
-        message = input(colored("\x1b[1;31m{}\x1b[0m: ".format(self.user), "green"))
-        self.pusher.trigger(self.chatroom, u'newmessage', {"user": self.user, "message": message})
+        message = input("\x1b[1;31m{}\x1b[0m: ".format(self.user))
+        if message == "":
+            sys.stdout.write(screen_code)
+        elif message != "":
+            self.pusher.trigger(self.chatroom, u'newmessage', {"user": self.user, "message": message})
+        else:
+            print(colored("Something went wrong", 'red'))
 
 
 if __name__ == "__main__":
